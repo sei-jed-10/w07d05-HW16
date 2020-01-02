5e970e2082cd9e03b52657d0c3f69077d25e534c
KOSaber <k.o.saber@hotmail.com>
2020-01-03 00:46:11 +0300

    done

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
-// Remember your promises
-// Go back to the lesson if you feel lost
-// Read the docs well, they usually have pretty well documented examples
-// Consider making your fetch work with a button
+
+
+function display(){
+   
+    axios({
+        method: 'get',
+        url: 'https://rickandmortyapi.com/api/character/1,2,3,4,5'
+    })
+    .then(response => { 
+        console.log(response);
+        
+for (i=0; i<5 ; i++){
+        console.log(response.data[i].name);
+        console.log(response.data[i].image);
+        document.getElementById("titlediv"+i).innerText = response.data[i].name;
+        document.getElementById("imgdiv"+i).innerHTML = "<img src=" + response.data[i].image +" width=200 >" ;
+
+    }
+    }).catch(error => {
+        console.log(error);
+    })
+
+
+}
