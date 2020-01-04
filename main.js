970e529f8c68f1d411e77235bd414e4d490ea870
Hanin Nouh <haninnouh@gmail.com>
2020-01-04 22:25:42 +0300

    Hanin Nouh HW16

--- main.js
+++ main.js
@@ -2,3 +2,34 @@
 // Go back to the lesson if you feel lost
 // Read the docs well, they usually have pretty well documented examples
 // Consider making your fetch work with a button
+
+//movies
+function Movies(){
+
+    axios({
+        method: 'get',
+        url:"https://ghibliapi.herokuapp.com/films"
+        })
+        .then(response =>{
+            if(document.getElementById('ShowMovie').hasChildNodes()==true){ 
+                document.getElementById('ShowMovie').innerHTML = '';
+                displayMovie(response);
+            }
+            else {
+                displayMovie(response);
+        }
+        })
+        .catch(error=>{
+        console.log(error)
+        })
+}
+
+function displayMovie(response){
+    for(var i=0;i<response.data.length;i++){                
+        var movie = document.getElementById('ShowMovie');
+        var divShow = document.createElement('div');
+        $(divShow).addClass("center");
+        divShow.innerHTML="<h1>"+response.data[i].title+"</h1><br><p>"+response.data[i].description+"</p><br>"  
+        movie.appendChild(divShow);        
+} 
+}
