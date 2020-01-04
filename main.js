ee234f04b9516f0768b93235588c14376fa1a85a
Hatim Alattas <56390393+hatimalattas@users.noreply.github.com>
2020-01-04 15:25:26 +0300

    Hatim Alattas

--- main.js
+++ main.js
@@ -2,3 +2,24 @@
 // Go back to the lesson if you feel lost
 // Read the docs well, they usually have pretty well documented examples
 // Consider making your fetch work with a button
+
+axios({
+    method: "get",
+    url: "https://rickandmortyapi.com/api/character/[1,2,3,4,5]",
+})
+.then(response => {
+console.log(response.data);
+    response.data.forEach((element) => {
+        $(".row").append(
+            `<div class='card' style='width: 18rem; display;inline-block'>
+            <div class='card-body'>
+              <h5 class='card-title'>${element.name}</h5>
+                <img class='card-img-top' id='char-img' src=${element.image}>
+                    </div>
+                        </div>`
+        )
+    });
+})
+.catch(error => {
+    console.log(error);
+});
