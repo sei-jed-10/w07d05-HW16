e1d2731f87e3a9b3e2f4b8499355f8d02299df9b
afnan ahmed <masrahiafnan90@gmail.com>
2020-01-05 16:30:40 +0300

    Hw16

--- main.js
+++ main.js
@@ -2,3 +2,19 @@
 // Go back to the lesson if you feel lost
 // Read the docs well, they usually have pretty well documented examples
 // Consider making your fetch work with a button
+axios({
+    method: 'get',
+    url: 'https://ghibliapi.herokuapp.com/films'
+    })
+    .then(response => {
+        console.log(response);
+        for(i = 0; i < 5; i++){
+            console.log(response.data[i].title);
+            console.log(response.data[i].description);
+            document.getElementById("title" + i).innerText = response.data[i].title;
+            document.getElementById("description" + i).innerText = response.data[i].description;
+        }
+    })
+    .catch(error => {
+        console.log(error);
+    })
